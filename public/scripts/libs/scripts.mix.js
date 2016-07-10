944ae7f0ee058f252b95e0bea4fbda82ae52fba5
George Rowe <georgehenryrowe@gmail.com>
2016-07-10 18:23:27 +0100

    New insta. Tried to use installed.js and failed

--- public/scripts/libs/scripts.mix.js
+++ public/scripts/libs/scripts.mix.js
@@ -3,6 +3,7 @@
 @import "picture.js";
 @import "jquery.fitvids.min.js";
 @import "howler.min.js";
+@import "instafeed.min.js"
 // @import "classie.js";
 // @import "demo1.js";
 // @import "modernizr.custom.js";
